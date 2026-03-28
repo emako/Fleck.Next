13bdf5be71df8f6841f1cb48ac7a0a2679fc2bdf
ema <mccoy39082@163.com>
2026-03-28 15:05:42 +0800

    Convert projects to SDK-style and update samples

--- src/Samples/WinFormsExample/MainForm.cs
+++ src/Samples/WinFormsExample/MainForm.cs
@@ -4,50 +4,49 @@ using System.Windows.Forms;
 
 namespace Fleck.WinFormsExample
 {
-	public partial class MainForm : Form
-	{
-		private readonly IList<WebSocketConnection> _allSockets = new List<WebSocketConnection>();
+    public partial class MainForm : Form
+    {
+        private readonly IList<IWebSocketConnection> _allSockets = new List<IWebSocketConnection>();
 
-		public MainForm()
-		{
-			InitializeComponent();
-			SetupServer();
-		}
+        public MainForm()
+        {
+            InitializeComponent();
+            SetupServer();
+        }
 
-		private void SetupServer()
-		{
-			var server = new WebSocketServer("ws://localhost:54321");
-			server.Start(c =>
-				{
-					c.OnOpen = () =>
-						{
-							AddItemOnUIThread("Open!");
-							_allSockets.Add(c);
-						};
-					c.OnClose = () =>
-						{
-							AddItemOnUIThread("Closed!");
-							_allSockets.Remove(c);
-						};
-					c.OnMessage = m => AddItemOnUIThread(m);
-				});
-		}
+        private void SetupServer()
+        {
+            var server = new WebSocketServer("ws://localhost:54321");
+            server.Start(c =>
+                {
+                    c.OnOpen = () =>
+                        {
+                            AddItemOnUIThread("Open!");
+                            _allSockets.Add(c);
+                        };
+                    c.OnClose = () =>
+                        {
+                            AddItemOnUIThread("Closed!");
+                            _allSockets.Remove(c);
+                        };
+                    c.OnMessage = m => AddItemOnUIThread(m);
+                });
+        }
 
-		private void AddItemOnUIThread(string message)
-		{
-			Invoke(new Action(() => lbxInput.Items.Add(message)));
-		}
+        private void AddItemOnUIThread(string message)
+        {
+            Invoke(new Action(() => lbxInput.Items.Add(message)));
+        }
 
+        private void OnSendClick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOutput.Text))
+                return;
 
-		private void OnSendClick(object sender, EventArgs e)
-		{
-			if (string.IsNullOrEmpty(txtOutput.Text))
-				return;
-
-			foreach (WebSocketConnection socket in _allSockets)
-			{
-				socket.Send(txtOutput.Text);
-			}
-		}
-	}
+            foreach (WebSocketConnection socket in _allSockets)
+            {
+                socket.Send(txtOutput.Text);
+            }
+        }
+    }
 }
